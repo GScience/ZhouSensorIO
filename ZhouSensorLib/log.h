6db866cca59a6734205a2337b8a5c6ccec673ce6
GScience Studio <GM2000@outlook.com>
2022-04-16 11:57:29 +0800

    修改日志输出格式

--- ZhouSensorLib/log.h
+++ ZhouSensorLib/log.h
@@ -2,8 +2,8 @@
 
 #define LOG_BUFFER_SIZE		1024
 
-#define LOGW(format, ...) OutputDebugStringfW(format, __VA_ARGS__)
-#define LOGA(format, ...) OutputDebugStringfA(format, __VA_ARGS__)
+#define LOGW(format, ...) OutputDebugStringfW("Sensor: "format, __VA_ARGS__)
+#define LOGA(format, ...) OutputDebugStringfA("Sensor: "format, __VA_ARGS__)
 
 #ifdef UNICODE
 #define LOG(format, ...) LOGW(format, __VA_ARGS__)
